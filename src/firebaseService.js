d1c6b72656867b08bc8b20f41155736f24b2e772
shubhampalgaian <shubhamm.pal@gaiansolutions.com>
2024-05-10 12:14:25 +0530

    FIREBASE UPDATING AND ADDING INSATNCE TO COLLECTION DONE

--- src/firebaseService.js
+++ src/firebaseService.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";import { getFirestore, collection, addDoc, doc, setDoc, getDocs,query, where } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAyezd2fLKRHaHQDFgBGtZGA4sLPa8n3jo",
@@ -15,16 +14,31 @@ const db = getFirestore(app);
 
 const firebaseService = {
   saveColumnsToFirestore: async (columns) => {
-    try {
+    try { 
       await Promise.all(columns.map(async (column) => {
-        const docRef = await addDoc(collection(db, "columns"), column);
-        console.log("Document written with ID: ", docRef.id);
+        // Check if document with same ID exists
+        const q = query(collection(db, "columns"), where("id", "==", column.id));
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.size > 0) {
+          // If document exists, update it
+          querySnapshot.forEach(async (doc) => {
+            await setDoc(doc.ref, column);
+            console.log("Document updated with ID: ", column.id);
+          });
+        } else {
+          // If document doesn't exist, add new document
+          const docRef = await addDoc(collection(db, "columns"), column);
+          console.log("New document added with ID: ", docRef.id);
+        }
       }));
       console.log("Data saved to Firebase successfully!");
     } catch (error) {
-      console.error("Error adding document: ", error);
+      console.error("Error adding/updating document: ", error);
     }
   }
 };
 
+
 export default firebaseService;
+
+
